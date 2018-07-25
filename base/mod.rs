403e5f173be6bbe0c3ded226b9128486a7c43af3
Maxym Naumchyk <max.naumch@gmail.com>
2018-07-26 00:37:28 +0300

    Impl Object + new ObjectTransaction

--- base/mod.rs
+++ base/mod.rs
@@ -20,3 +20,75 @@ pub struct Package {
 /// this service can carry out.
 pub struct Service {
 }
+
+/// Transaction allows making multiple changes to object as a single
+/// operation. This allows to revert changes if one of the changes
+/// fails.
+pub struct ObjectTransaction {
+}
+
+impl Object {
+
+    /// All public services at current network level of the object.
+    pub fn public_services(&self) {
+        unimplemented!()
+    }
+
+    /// All internal services at current network level of the object.
+    pub fn internal_services(&self) {
+        unimplemented!()
+    }
+
+    /// All private services at current network level of the object.
+    pub fn private_services(&self) {
+        unimplemented!()
+    }
+
+    /// All interfaces implemented by this object.
+    pub fn interfaces(&self) {
+        unimplemented!()
+    }
+
+    pub fn apply_transaction(&mut self) {
+        unimplemented!()
+    }
+}
+
+impl ObjectTransaction {
+
+    pub fn new() {
+        unimplemented!()
+    }
+
+    /// Add new public service.
+    pub fn add_public_service(&mut self) {
+        unimplemented!()
+    }
+
+    /// Add new internal service.
+    pub fn add_internal_service(&mut self) {
+        unimplemented!()
+    }
+
+    /// Add new private service.
+    pub fn add_private_service(&mut self) {
+        unimplemented!()
+    }
+
+    pub fn remove_public_service(&mut self) {
+        unimplemented!()
+    }
+
+    pub fn remove_internal_service(&mut self) {
+        unimplemented!()
+    }
+
+    pub fn remove_private_service(&mut self) {
+        unimplemented!()
+    }
+
+    /// Mark this object as interface implementer.
+    pub fn implement_interface(&mut self) {
+        unimplemented!()
+    }
+}
